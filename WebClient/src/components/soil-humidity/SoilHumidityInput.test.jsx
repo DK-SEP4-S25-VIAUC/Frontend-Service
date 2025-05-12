9c3ef92b2da48e255590e95a89ff547cab0ce418
Mikkel077 <mikkelhansen077@gmail.com>
2025-05-12 09:14:00 +0000

    fixed eslint errors

--- WebClient/src/components/soil-humidity/SoilHumidityInput.test.jsx
+++ WebClient/src/components/soil-humidity/SoilHumidityInput.test.jsx
@@ -1,5 +1,5 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {render, screen, fireEvent, waitFor, act} from "@testing-library/react";
+import {render, screen, fireEvent, waitFor} from "@testing-library/react";
 import axios from "axios";
 import SoilHumidityInput from "./SoilHumidityInput.jsx";
 
